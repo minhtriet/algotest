de1ade5c9ed045beff9ac3f5ac12b961b43fdadf
minhtriet <minhtriet09@gmail.com>
2023-02-13 10:50:58 +0000

    add test code

---
+++ TreeTest/BinaryTreeTest.cs
@@ -0,0 +1,23 @@
+﻿namespace TreeTest;
+using AlgoTest;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestAdd()
+    {
+        AlgoTest.BinaryTree tree = new AlgoTest.BinaryTree();
+        tree.Add("g");
+        tree.Add("b");
+        tree.Add("c");
+        tree.Add("z");
+        tree.Add("a");
+        Node root = tree.Root;
+        Assert.AreEqual(root.Data, "g");
+        Assert.AreEqual(root.LeftNode.Data, "b");
+        Assert.AreEqual(root.RightNode.Data, "z");
+        Assert.AreEqual(root.LeftNode.LeftNode.Data, "a");
+        Assert.AreEqual(root.LeftNode.RightNode.Data, "c");
+    }
+}
