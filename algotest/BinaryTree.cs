8d37c1c17279867d52de8ddbe530d9bc1d55ccef
minhtriet <minhtriet09@gmail.com>
2023-02-13 10:27:02 +0000

    add cs files

---
+++ algotest/BinaryTree.cs
@@ -0,0 +1,57 @@
+﻿namespace AlgoTest
+{
+    public class Node
+    {
+        public Node LeftNode { get; set; }
+        public Node RightNode { get; set; }
+        public string Data { get; set; }
+    }
+
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+
+        public bool Add(string value)
+        {
+            Node newNode = new Node();
+            newNode.Data = value;
+
+            if (this.Root == null) {
+                this.Root = newNode;
+                return true;
+            }
+
+            Node before = null, after = this.Root;
+
+            while (after != null)
+            {
+                before = after;
+                int compare_after = value.CompareTo(after.Data);
+                if (compare_after < 0)
+                    after = after.LeftNode;
+                else if (compare_after > 0)
+                    after = after.RightNode;
+                else
+                    return false;   // same value
+            }
+           
+            int compare_before = value.CompareTo(before.Data);
+            if (compare_before < 0)
+                before.LeftNode = newNode;
+            else
+                before.RightNode = newNode;
+
+            return true;
+        }
+
+
+        public void TraversePreOrder(Node parent) {
+            if (parent != null) {
+                Console.WriteLine(parent.Data);
+                TraversePreOrder(parent.LeftNode);
+                TraversePreOrder(parent.RightNode);
+            }
+        }
+    }
+}
+
---
+++ algotest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+namespace AlgoTest { 
+
+    class Program { 
+        static void Main(string[] args) {
+            string filename = "";
+            if (args.Length != 1) {
+                Console.WriteLine("No input filename found, default to [algorithms part dataset.csv]");
+                filename = "algorithms part dataset.csv";
+            } else { 
+                filename = args[0];
+            }
+            if (!File.Exists(filename)) {
+                Console.WriteLine($"File {filename} not found");
+                return;
+            }
+            int hasDup = 0;
+            BinaryTree binaryTree = new BinaryTree();
+            using (StreamReader reader = new StreamReader(filename)) {
+                string headerLine = reader.ReadLine();
+                while (!reader.EndOfStream) {
+                    string line = reader.ReadLine();
+                    if (line != null)
+                        if (!binaryTree.Add(line))
+                        {
+                            if (hasDup == 0) 
+                                Console.WriteLine("DUPLICATE LINE(S):");
+                            hasDup += 1;
+                            Console.WriteLine(line);
+                        }
+                }
+            }
+            Console.WriteLine($"There are {hasDup} duplicates.");
+            //binaryTree.TraversePreOrder(binaryTree.Root);
+            //Console.WriteLine();
+        }
+    }
+}
+
